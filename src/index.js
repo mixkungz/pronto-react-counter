239cfca9816f123a4118628794c9c918c1f0160d
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-09 20:28:25 +0700

    :sparkles: Make app to be a provider and initial state and function to update state.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
@@ -6,14 +6,32 @@ import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import { rootReducers } from './reducers/index'
+import { AppContext } from './contexts'
+import { devToolsEnhancer } from 'redux-devtools-extension'
+// Redux
+const store = createStore(rootReducers, { counter: 0 }, devToolsEnhancer())
 
-const store = createStore(rootReducers)
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+class CounterApp extends Component {
+  state = {
+    counter: 1,
+    increaseCounter: () =>
+      this.setState(prevState => ({ counter: prevState.counter + 1 })),
+    decreaseCounter: () =>
+      this.setState(prevState => ({ counter: prevState.counter - 1 }))
+  }
+
+  render() {
+    return (
+      <AppContext.Provider value={this.state}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </AppContext.Provider>
+    )
+  }
+}
+
+ReactDOM.render(<CounterApp />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
