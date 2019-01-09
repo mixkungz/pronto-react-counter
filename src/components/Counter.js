6ecfb20104913dde8f960f41591d9fd1f1fe89a2
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-09 15:50:13 +0700

    :sparkles: Add counter component.

---
+++ src/components/Counter.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react"
+
+class Counter extends Component {
+  state = {
+    counter: 0
+  }
+
+  increaseCounter = () => {
+    let { counter } = this.state
+    this.setState({
+      counter: counter + 1
+    })
+  }
+  decreaseCounter = () => {
+    let { counter } = this.state
+    this.setState({
+      counter: counter - 1
+    })
+  }
+
+  render() {
+    const { counter } = this.state
+    return (
+      <div>
+        <h3>Counter : {counter}</h3>
+        <button onClick={this.decreaseCounter}>-</button>
+        <button onClick={this.increaseCounter}>+</button>
+      </div>
+    )
+  }
+}
+
+export { Counter }
