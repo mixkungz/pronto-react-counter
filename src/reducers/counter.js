f429b1cf73d6ac744da4fa11a80285dab7bdae18
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-09 17:13:28 +0700

    :bug: Fix not read a action type.

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -3,7 +3,7 @@ import { INCREASE_COUNTER, DECREASE_COUNTER } from '../actions/types'
 const initialState = 0
 
 const counterReducer = (state = initialState, actions) => {
-  switch (actions) {
+  switch (actions.type) {
     case INCREASE_COUNTER:
       return state + 1
     case DECREASE_COUNTER:
